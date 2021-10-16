bfd72e0ecae22256d4cbdadada2e056e22f04686
Alban <alban.pezzoli@gmail.com>
2021-10-16 13:54:48 +0200

    dev: Added some testCase on services, DTO mapping, and ViewModel

--- TheForkTechnicalTest/TheForkTechnicalTestTests/TheForkTechnicalTestTests.swift
+++ TheForkTechnicalTest/TheForkTechnicalTestTests/TheForkTechnicalTestTests.swift
@@ -18,16 +18,80 @@ class TheForkTechnicalTestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testRetriveDataFromMockedService() {
+        RestaurantServiceMock().retriveListRestaurant { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(true)
+            case .failure:
+                XCTFail()
+            }
         }
     }
+    
+    func testTransformDataServiceToDTO() {
+        var restaurantDTOArray: [RestaurantDTO] = []
+        RestaurantServiceMock().retriveListRestaurant { result in
+            switch result {
+            case .success(let response):
+                for restaurant in response.data {
+                    restaurantDTOArray.append(RestaurantDTO(restaurant: restaurant))
+                }
+                XCTAssertTrue(restaurantDTOArray.count == response.data.count)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func testDTOMappingData() {
+        RestaurantServiceMock().retriveListRestaurant { result in
+            switch result {
+            case .success(let response):
+                for restaurant in response.data {
+                    let dtoToTest = RestaurantDTO(restaurant: restaurant)
+                    
+                    XCTAssertEqual(dtoToTest.address, "\(restaurant.address.street)\n\(restaurant.address.postalCode) \(restaurant.address.locality) \(restaurant.address.country)")
+                    XCTAssertEqual(dtoToTest.theForkRating, restaurant.aggregateRatings.thefork.ratingValue)
+                    XCTAssertEqual(dtoToTest.theForkReviewNumbers, restaurant.aggregateRatings.thefork.reviewCount)
+                    XCTAssertEqual(dtoToTest.tripadvisorRating, restaurant.aggregateRatings.tripadvisor.ratingValue)
+                    XCTAssertEqual(dtoToTest.tripadvisorReviewNumbers, restaurant.aggregateRatings.tripadvisor.reviewCount)
+                    XCTAssertEqual(dtoToTest.name, restaurant.name)
+                    
+                }
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func testRestaurantVMRetrieveRestaurant() {
+        let vm = RestaurantsViewModel(restaurantListService: RestaurantServiceMock())
+        vm.delegate = self
+        vm.retrieveRestaurant()
+    }
+    
+    func testRestaurantVMSortByName() {
+        let vm = RestaurantsViewModel(restaurantListService: RestaurantServiceMock())
+        vm.delegate = self
+        vm.retrieveRestaurant()
+        vm.sortRestaurantByName()
+    }
+    
+    func testRestaurantVMSortByRatings() {
+        let vm = RestaurantsViewModel(restaurantListService: RestaurantServiceMock())
+        vm.delegate = self
+        vm.retrieveRestaurant()
+        vm.sortRestaurantByRank()
+    }
+}
 
+extension TheForkTechnicalTestTests: RestaurantServiceDelegate {
+    func retrieveRestaurantDidSuccess(restaurants: [RestaurantDTO]) {
+        XCTAssertTrue(true)
+    }
+    
+    func retrieveRestaurantDidFailed(error: Error) {
+        XCTFail()
+    }
 }
